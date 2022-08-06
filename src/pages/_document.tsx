6b97920677982e1814c8de1d6487969aae4a6d91
Maxsuel Lima <maxsuelslima@gmail.com>
2022-08-05 20:41:43 -0400

    Meta added.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-title-in-document-head */
 import { Head, Html, Main, NextScript } from "next/document"
 
 const MyDocument = () =>{
@@ -14,6 +15,28 @@ const MyDocument = () =>{
         <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png"/>
         <link rel="favicon-16x16" sizes="16x16" href="/favicon/favicon-16x16.png"/>
         <link rel="favicon-32x32" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+
+        <title>PREAMAR</title>
+        <meta name="title" content="PREAMAR"/>
+        <meta name="description" content="PREAMAR é um projeto de ações artísticas em rede a partir do Maranhão."/>
+        <meta name="keywords" content=" preamar, arte, maranhão, são luis, alcântra, projetos, artísticos, chão slz, lima galeria, centro histórico, maranhão, artistas, obras, contemporânea"/>
+        <meta name="robots" content="index, follow"/>
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta name="language" content="Portuguese"/>
+
+
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://preamaremrede.org/"/>
+        <meta property="og:title" content="PREAMAR"/>
+        <meta property="og:description" content="PREAMAR é um projeto de ações artísticas em rede a partir do Maranhão."/>
+        <meta property="og:image" content="/meta-photo.png"/>
+
+
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:url" content="https://preamaremrede.org/"/>
+        <meta property="twitter:title" content="PREAMAR"/>
+        <meta property="twitter:description" content="PREAMAR é um projeto de ações artísticas em rede a partir do Maranhão."/>
+        <meta property="twitter:image" content="/meta-photo.png"/>
         </Head>
         <NextScript />
         <body>
