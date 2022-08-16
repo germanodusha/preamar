8a739870e22c2ddd92096bc78bce9d83bf86e21b
Germano Dushá <germano@ofora.org>
2022-08-15 21:57:23 -0300

    Update editavel.tsx

--- editavel.tsx
+++ editavel.tsx
@@ -17,13 +17,13 @@ const firstSection =
             }
             description={
                 <>
-                <u>ARTISTAS</u><br/>
+                <i>ARTISTAS</i><br/>
                 CERAMISTAS DE ITAMATATIUA, CLÁUDIO COSTA, DINHO ARAÚJO, GÊ VIANA, GENILSON GUAJAJARA, INGRID BARROS, MÁRCIO VASCONCELOS, MARCOS FERREIRA, MARLENE BARROS, PABLO MONTEIRO, ROMANA MARIA, SILVANA MENDES, THIAGO FONSECA, THIAGO MARTINS DE MELO, TASSILA CUSTODES, TIETA MACAU, TON BEZERRA, VICENTE MARTINS JR.<br/>
-                <u>CURADORIA</u><br/>
+                <i>CURADORIA</i><br/>
                 SAMANTHA MOREIRA, FREDERICO SILVA, YURI LOGRADO, MARCO LIMA<br/>
-                <u>REALIZAÇÃO</u><br/>
+                <i>REALIZAÇÃO</i><br/>
                 CHÃO SLZ, LIMA GALERIA, CASA DO SEREIO<br/>
-                <u>PARCERIA</u><br/>
+                <i>PARCERIA</i><br/>
                 DEPARTAMENTO DE ARTES VISUAIS DA UNIVERSIDADE FEDERAL DO MARANHÃO - UFMA
                 </>
             }/>            
@@ -40,11 +40,11 @@ const secondSection =
             </>
             }
             description={
-            <><u>RESIDÊNCIA 1</u><br/>
+            <><i>RESIDÊNCIA 1</i><br/>
             SILVANA MENDES E TASSILA CUSTODES<br/>
-            <u>RESIDÊNCIA 2</u><br/>
+            <i>RESIDÊNCIA 2</i><br/>
             MARCOS FERREIRA<br/>
-            <u>RESIDÊNCIA 3</u><br/>
+            <i>RESIDÊNCIA 3</i><br/>
             THIAGO MARTINS DE MELO E GERMANO DUSHÁ
             </>
             }/>   
@@ -64,8 +64,8 @@ const thirdSection =
             }
             description={
                 <>
-                    <u>COLABORAÇÕES E COAUTORIAS ENTRE ARTISTAS E CURADORES</u> COM RAFAEL RG, THIAGO MARTINS DE MELO E GERMANO DUSHÁ <br/>
-                    <u>CIRCULAÇÃO E MERCADO DE ARTE</u> COM GERMANO DUSHÁ
+                    <i>COLABORAÇÕES E COAUTORIAS ENTRE ARTISTAS E CURADORES</i> COM RAFAEL RG, THIAGO MARTINS DE MELO E GERMANO DUSHÁ <br/>
+                    <i>CIRCULAÇÃO E MERCADO DE ARTE</i> COM GERMANO DUSHÁ
                 </>
             }/>
 
@@ -82,10 +82,10 @@ const fourthSection =
             }
             description={
              <>
-                <u>ARTISTAS</u><br/>
+                <i>ARTISTAS</i><br/>
             SILVANA MENDES, DINHO ARAÚJO, MARCIO VASCONCELOS
             <br/>
-                <u>CURADORIA</u><br/>
+                <i>CURADORIA</i><br/>
                 SAMANTHA MOREIRA, FREDERICO SILVA, YURI LOGRADO, MARCO LIMA, GERMANO DUSHÁ
                 </>
             }/>   
